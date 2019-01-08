d4386171a4c3123921545290a7b8fa07f2de91a2
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-01-08 11:05:12 -0200

    Fixed getter

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,24 +1,23 @@
 class CalcController {
   constructor() {
     // Fetch calculator objects
-    this.timeEl = document.querySelector("#hora");
-    this.dateEl = document.querySelector("#data");
-    this.displayCalcEl = document.querySelector("#display");
-    this._currentDate;
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
     this.initialize();
   }
 
   initialize() {
-    this.dateEl.innerHTML = "28/04/1994";
-    this.timeEl.innerHTML = "23:59";
+    this._dateEl.innerHTML = "28/04/1994";
+    this._timeEl.innerHTML = "23:59";
   }
 
   get displayCalc() {
     return this._displayCalcEl.innerHTML;
   }
 
-  set displayCalc(valor) {
-    this._displayCalcEl.innerHTML = valor;
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
   }
 
   get currentDate() {
